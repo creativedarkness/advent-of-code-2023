879a8abc06a07c6795b55d57538e78b59699214a
Melissa Schultz <melissaschultz@northwesternmutual.com>
2023-12-01 13:04:10 -0600

    Add Day 1 part 2

--- Day1/index.js
+++ Day1/index.js
@@ -20,4 +20,52 @@ exports.partOne = function (file) {
   return sum
 }
 
-console.log(exports.partOne('input.txt'))
+const numbersMap = {
+  on: 1,
+  tw: 2,
+  th: 3,
+  fo: 4,
+  fi: 5,
+  si: 6,
+  se: 7,
+  ei: 8,
+  ni: 9
+}
+
+const searchTerms = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+// two1nine // two 1 nine
+// eightwothree // eight two three
+// abcone2threexyz // one 2 three
+// xtwone3four // two one 3 four
+// 4nineeightseven2 // four nine eight seven 2
+// zoneight234 // one eight 2 3 4
+// 7pqrstsixteen // 7 six sixteen
+
+exports.partTwo = function (file) {
+  const input = fs.readFileSync(path.resolve(__dirname, `./${file}`), 'utf-8').toString().split('\n') // read file and split by line
+  let sum = 0
+
+  // input.forEach((line) => {
+  //   const digits = line.split('').map(Number).filter(Boolean) // split each line into an array of digits
+  //   const first = digits[0]
+  //   const last = digits[digits.length - 1]
+
+  //   sum += parseInt(`${first}${last}`)
+  // })
+
+  input.forEach((line) => {
+    const words = line.split(' ')
+    const numbers = words.filter((word) => {
+      return numbersMap[word]
+    })
+    const first = numbers[0]
+    const last = numbers[numbers.length - 1]
+    console.log(words, numbers)
+    sum += parseInt(`${first}${last}`)
+  })
+
+  return sum
+}
+
+// console.log(exports.partOne('partOneInput.txt'))
--- Day1/index.spec.js
+++ Day1/index.spec.js
@@ -1,10 +1,23 @@
 const { partOne } = require('./index')
+const { partTwo } = require('./index')
 
-describe('partOne', () => {
-  it('returns the sum of the first and last digit of each line in the input', () => {
-    const expected = 142
-    const actual = partOne('testData.txt')
+describe('Day 1', () => {
+  describe('partOne', () => {
+    it('returns the sum of the first and last digit of each line in the input', () => {
+      const expected = 142
+      const actual = partOne('partOneTestData.txt')
 
-    expect(actual).toEqual(expected)
+      expect(actual).toEqual(expected)
+    })
   })
+
+  describe.only('partTwo', () => {
+    it('returns the sum of the first and last digit of each line in the input', () => {
+      const expected = 281
+      const actual = partTwo('partTwoTestData.txt')
+
+      expect(actual).toEqual(expected)
+    })
+  })
+
 })
